1a83e02bf181a4375902af54a9f579c72bcdd2bb
RKrueger <student@rkrueger.local>
2017-02-22 09:26:02 -0600

    set up table view

--- PhotoNotes/ViewController.swift
+++ PhotoNotes/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
 
+    @IBOutlet weak var folderTableView: UITableView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "myCell")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 5
+    }
 }
 
