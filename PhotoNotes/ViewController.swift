b45e70339163d725a1d69cf42bf7a71d6596fde1
RKrueger <student@rkrueger.local>
2017-02-22 09:16:51 -0600

    Initial Commit

---
+++ PhotoNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoNotes
+//
+//  Created by rkalvani on 2/21/17.
+//  Copyright © 2017 rkalvani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+}
+
